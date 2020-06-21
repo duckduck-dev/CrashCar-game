53ce06d2cecd0e9a558522b0ac67f19c04d23303
jellyBean-sys <67203625+jellyBean-sys@users.noreply.github.com>
2020-06-21 16:46:17 +0530

    Add files via upload

---
+++ loginPage/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      username: '',
+      show: true
+     };
+  }
+  mySubmitHandler = (e) => {
+    e.preventDefault();
+    document.getElementById('box1').value= '';
+    document.getElementById('box2').value= '';
+    document.getElementById('mainContainer');
+    alert("Login successfull!!! ");
+  }
+
+  validatePassword = (e) => {
+    let val = e.target.value;
+    if (!Number(val)) {
+      alert(' Invalid mobile number');
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.mySubmitHandler}>
+      <main id="container">
+      <div id="mainContainer">
+      <p>Welcome Back!!!</p>
+      <input id="box1" type='text' placeholder='Username' name="username" required/><br />
+      <input id="box2" type='password' placeholder="Password" required/><br />
+      <input id="signUp" type="submit" value="Login"/>
+      <a href="#">forgot password?</a>
+      </div>
+      </main>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<MyForm />, document.getElementById('root'));
